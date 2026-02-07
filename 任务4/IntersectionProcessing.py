986a0c53e5b96380fbcf161ca6d48453f884cdc6
OswaldReijngoud <1322343923@qq.com>
2026-02-07 09:59:59 +0800

    complete task4

--- 任务4/IntersectionProcessing.py
+++ 任务4/IntersectionProcessing.py
@@ -14,9 +14,10 @@ Crossroad State Machine Framework.
 Code Structure:
 Point:Basic data structure
 Track:Track analysis (boundary line/center line), and some simple track character analysis (longest white line/lost line)
-Cross:Handle the condition of crossing
+Cross:Handle crossing scenario
 Visualize:Visualize special line and some text of data characteristics
-Main:Orchestrates the entire code; Control the play of the video
+Main:Orchestrates the entire code; Control video playback
+Utils:Tools
 '''
 
 # 1.Define a 2D coordinate class.
@@ -29,13 +30,17 @@ class Point:
         # Convert point (row,col) to (x,y)
         return self.col,self.row
 
+    @staticmethod
+    def get_midpoint(p1,p2):
+        return Point((p1.row+p2.row)//2,(p1.col+p2.col)//2)
+
 # 2.Responsible for longest white line,boundary lines and the centerline.
 class Track:
     def __init__(self):
 
         # About crop
-        self.up_chop_rate=0     #Proportion of the top to be cropped
-        self.down_chop_rate=0.3   #Proportion of the bottom to be cropped
+        self.up_crop_rate=0     # Top crop ratio
+        self.down_crop_rate=0.2   # Bottom crop ratio
 
         # Edge point sets for left and right track boundaries
         self.LeftPoints=[]
@@ -46,7 +51,7 @@ class Track:
         self.RightPoints_LostNum=0
         self.LeftPoints_LostFlag=0#0 not lost;1 lost
         self.RightPoints_LostFlag=0
-        self.LostThreshold=0.2
+        self.LossThreshold=0.2
 
         # About center points
         self.CenterPoints=[]        #Set of points for the centerline
@@ -70,7 +75,7 @@ class Track:
         self.left_corner_index=None  # The index of the lower corners on the left side line
         self.right_corner_index=None
 
-        # Use to find upper corners
+        # Used to find upper corners
         self.ScanLeftPoints=None
         self.ScanRightPoints=None
 
@@ -103,8 +108,8 @@ class Track:
         """
         # 获取视频帧的高度和宽度，之后摄像头图像高度和宽度应该会给，这里就先直接从视频获取了
         height,width=frame.shape[:2]
-        start_row=int(height*self.up_chop_rate)
-        end_row=int(height*(1-self.down_chop_rate))
+        start_row=int(height * self.up_crop_rate)
+        end_row=int(height * (1 - self.down_crop_rate))
         return frame[start_row:end_row,:]
 
     def find_start_line(self, binary,h,w):
@@ -121,7 +126,7 @@ class Track:
         self.CenterPoints.clear()
 
         min_valid_width=50   #Minimum width threshold for valid blocks (noise filtering)
-        # Search white pixel in the edge of the track, search from the column of the longest white line
+        # Search for white pixel on the edge of the track, search from the column of the longest white line
         search_limit=int(h*2/3)
         # Use Longest_White_Line_Top_Point.col as anchor_col if available, or use the center of the image
         if self.Longest_White_Line_Top_Point is not None:
@@ -140,7 +145,7 @@ class Track:
             # Find right edge point
             points_right_to_the_anchor_col=binary[row,anchor_col:]
             right_black_indices=np.where(points_right_to_the_anchor_col==0)[0]
-            if (len(right_black_indices)>0):
+            if len(right_black_indices)>0:
                 r_idx=anchor_col+right_black_indices[0]-1
             else:   r_idx=w-1
             # Validate the width and set the start line
@@ -192,7 +197,7 @@ class Track:
         if self.start_row is None:#若没有起始行，就直接返回
             return
         h,w=binary.shape  # 得出高宽，防越界
-        #Use "visited" to ensure the search direction is always moving forward and avoids cycling.
+        #Use "visited" to prevent backtracking and avoid cycling.
         visited=np.zeros_like(binary, np.uint8)#0 unvisited;1 visited
         directions_l = np.array([#右->右上->上->左上->左->左下->下->右下
             [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1], [1, 0], [1, 1]
@@ -208,7 +213,7 @@ class Track:
         visited[cen_row,cen_col]=1
         while cen_row>0 and count<max_iteration: # Search all the way to the top;maximum iteration protection
             count+=1
-            found=False#found是是否找到下一个点的flag
+            found=False # Indicate if the next point is found
             for direction in range(8):
                 #取出变化量数组里的值
                 delta_row0,delta_col0=directions_l[direction]
@@ -234,7 +239,7 @@ class Track:
         # 搜右边线
         #The `visited` set is NOT reset here, because left and right boundary usually do not overlap
         #Only when the track is too narrow or in some extreme conditions, these two lines will get in touch
-        #This method prevents boundary line from crisscrossing(单例标记保护 visited-set synchronization)
+        #This method prevents boundary line from crossing(单例标记保护 visited-set synchronization)
         cen_row, cen_col = self.start_row, self.start_right
         visited[cen_row, cen_col] = 1
         count=0#Reset counter
@@ -263,7 +268,7 @@ class Track:
     def detect_lost_line(self,h,w):
         self.LeftPoints_LostFlag = 0
         self.RightPoints_LostFlag=0
-        # Calculate lost points
+        # Calculate line loss metrics
         # We expect boundary points num is roughly the image height
         if self.start_row:
             expected_points=self.start_row
@@ -273,13 +278,13 @@ class Track:
         self.RightPoints_LostNum=max(0,expected_points-len(self.RightPoints))
         if expected_points>0:
             if (len(self.LeftPoints)==0                                    or   # Do not find boundary
-                self.LeftPoints_LostNum/expected_points>self.LostThreshold or   # Not enough points
+                self.LeftPoints_LostNum/expected_points>self.LossThreshold or   # Not enough points
                 self.LeftPoints[-1].row>2/3*h                              or   # Ends too early
                 self.LeftPoints[-1].col==0                                      # Ends at side boundary
             ):
                 self.LeftPoints_LostFlag=1
             if (len(self.RightPoints)==0                                     or
-                self.RightPoints_LostNum/expected_points>self.LostThreshold or
+                self.RightPoints_LostNum/expected_points>self.LossThreshold or
                 self.RightPoints[-1].row>2/3*h                              or
                 self.RightPoints[-1].col==w-1
             ):
@@ -341,7 +346,7 @@ class Track:
         # h,w=binary.shape[:2]
         # if not self.LeftPoints_LostFlag or not self.RightPoints_LostFlag or self.Longest_White_Line_Length<0.6*h:
         #     return
-        # #It makes no sense when lost line is too severe
+        # #Unreliable when lost line is too severe
         # if (self.LeftPoints_LostNum>0.9*h or
         #     self.RightPoints_LostNum>0.9*h or
         #     len(self.LeftPoints)<20 or
@@ -560,28 +565,6 @@ class Track:
     #     self.find_down_corners(h,w)
     #     self.find_up_corners(binary,h,w)
 
-    def bezier_fit(self,input_points,dt=0.01):
-           #
-           # 贝塞尔曲线核心函数
-           # 根据四个控制点生成三次曲线
-           #  Parameters:
-           #     input_points: 输入特征点
-           #  Returns:
-           #     output: 贝塞尔拟合后的点列表
-           #
-        output=[]
-        #检验输入
-        if len(input_points)!=4:
-            print("控制点数量错误")
-            return output
-        t=0
-        while t<=1.0+1e-6:#每个t对应图上一个点,dt控制平滑度,+1e-6是为了确保包含终点（t可能在0.99时t+dt=1.0000000001，跳过t=1.0）
-            center_row= (1 - t) ** 3 * input_points[0].row + 3 * (1 - t) ** 2 * t * input_points[1].row + 3 * (1 - t) * t ** 2 * input_points[2].row + t ** 3 * input_points[3].row
-            center_col= (1 - t) ** 3 * input_points[0].col + 3 * (1 - t) ** 2 * t * input_points[1].col + 3 * (1 - t) * t ** 2 * input_points[2].col + t ** 3 * input_points[3].col
-            output.append(Point(round(center_row), round(center_col)))
-            t+=dt
-        return output
-
     def generate_bezier_center(self,h,w):
         #函数功能：生成贝塞尔拟合中心线
         #h和w是图像的高和宽，用来防越界
@@ -589,15 +572,20 @@ class Track:
         if len(self.LeftPoints)<1 or len(self.RightPoints)<1:return
         def get_three_part_points(points):
             #函数功能：返回首点、尾点、三等分点
-            #valid_points =[p for p in points if 0 <= p.row < h and 0 <= p.col < w]#过滤掉超出图像范围的点
+            n = len(points)
+            # extracted_points=[]
+            # index=[0,n//3,2*n//3,-1]
+            # for idx in index:
+            #     p=points[idx]
+            #     p_clipped_row=int(np.clip(p.row,0,h-1))
+            #     p_clipped_col=int(np.clip(p.col,0,w-1))
+            #     extracted_points.append(Point(p_clipped_row,p_clipped_col))
+            # return extracted_points
             for p in points:
                 p.row=int(np.clip(p.row,0,h-1))
                 p.col=int(np.clip(p.col,0,w-1))
-            n = len(points)
+            return [points[0],points[n//3],points[2*n//3],points[-1]]
 
-            return [
-                points[0],points[n//3], points[2*n//3],points[-1]
-            ]
         left_feature=get_three_part_points(self.LeftPoints)
         right_feature=get_three_part_points(self.RightPoints)
         self.bezier_input=[]
@@ -605,10 +593,10 @@ class Track:
             mid_row=(l_p.row+r_p.row)/2
             mid_col=(l_p.col+r_p.col)/2
             #控制点也要防越界，但控制点必须确保为4个，所以不能直接过滤
-            mid_row = max(0, min(round(mid_row), h - 1))
-            mid_col = max(0, min(round(mid_col), w - 1))
+            mid_row=round(np.clip(mid_row,0,h-1))
+            mid_col=round(np.clip(mid_col,0,w-1))
             self.bezier_input.append(Point(round(mid_row),round(mid_col)))
-        self.CenterPoints=self.bezier_fit(self.bezier_input)
+        self.CenterPoints=Utils.bezier_fit(self.bezier_input)
 
     def _truncate_track_line(self):
         # Remove the points after the corner, they are horizontal line of the crossroad
@@ -685,7 +673,7 @@ class Cross:
         self.step=self.CrossStep.NONE
         self.mode=self.CrossMode.NONE
         self.visualization_points=[]
-        self.track_half_width=0 # TODO: Dynamically determine the track_half_width
+        self.track_half_width=0
         self.debug_info={}
         self._exit_clk=0
 
@@ -698,13 +686,13 @@ class Cross:
                 track.is_external_control=True  # Tracker no longer control line generation
                 self._exit_clk=0
                 self.step=self.CrossStep.Fix
-                self.mode=self._determine_mode(track)
-                self._patch_lines(track)
+                self.mode=self._determine_mode(track,analyse)
+                self._patch_lines(h,w,track)
                 return True
             track.is_external_control=False
             return False
         elif self.step==self.CrossStep.Fix:
-            self._patch_lines(track)
+            self._patch_lines(h,w,track)
             self._exit_clk+=1
             if self._check_exit(h,w,track, analyse):
                 track.is_external_control=False
@@ -718,30 +706,87 @@ class Cross:
     def _check_entry(self,h,w,track,analyse):
         # Standard: Both left and right lost line/ Find at least one corner/ Vision is not too narrow
         lost_line=track.LeftPoints_LostFlag and track.RightPoints_LostFlag
-        board_view=track.Longest_White_Line_Length>h*0.5
+        broad_view= track.Longest_White_Line_Length > h * 0.5
         #When the line is short, the variance method is inaccurate and should only be used as a reference.
         big_side_var=analyse.sigma_left>50 and analyse.sigma_right>50
         down_corners_exist=track.LeftDownCorner is not None or track.RightDownCorner is not None
 
-        should_enter= lost_line and board_view and down_corners_exist
+        should_enter= lost_line and broad_view and down_corners_exist
 
         # Save debug info
         self.debug_info={
             "State":"OutsideCross",
             "BothLost":lost_line,
-            "OpenView":board_view,
+            "OpenView":broad_view,
             "CornExist":down_corners_exist,
             "BigVar":big_side_var,
             "Enter":should_enter
         }
         return should_enter
 
-    def _determine_mode(self,track):
-        # TODO
-        pass
+    @staticmethod
+    def _determine_mode(track,analyse):
+        # Prioritize the side where the corner is detected
+        if (track.LeftDownCorner and track.RightDownCorner and
+                0.5*analyse.sigma_right<analyse.sigma_left<2*analyse.sigma_right):
+            return Cross.CrossMode.Straight
+        if track.LeftDownCorner is not None and track.LeftPoints_LostFlag:
+            return Cross.CrossMode.Left
+        elif track.RightDownCorner is not None and track.RightPoints_LostFlag:
+            return Cross.CrossMode.Right
+        else:   #TODO: We can do sth special when nothing fit
+            return Cross.CrossMode.Straight
 
-    def _patch_lines(self,track):
-        # TODO
+    def _patch_lines(self,h,w,track):
+        # Determine the width of the track
+        if track.start_flag:
+            self.track_half_width=(track.start_right-track.start_left)//2
+        else:   self.track_half_width=w*0.4
+
+        # Determine control points:p0(start) p1(corner) p3(End) p2(mid point of p1 and p3)
+        p0=p1=p2=p3=None
+
+        # Determine p0
+        if track.start_flag:
+            p0 =Point.get_midpoint(track.LeftPoints[0],track.RightPoints[0])
+        else:
+            p0 = Point(h-1, w//2)
+
+        # Determine p1
+        # Find corner(s)->use current corner to determine p1
+        if self.mode==self.CrossMode.Straight and track.LeftDownCorner and track.RightDownCorner:
+            p1=Point.get_midpoint(track.LeftDownCorner,track.RightDownCorner)
+        elif self.mode==self.CrossMode.Left and track.LeftDownCorner:
+            p1=Point(track.LeftDownCorner.row,
+                     track.LeftDownCorner.col
+                     +0.9*self._get_offset(track,track.LeftDownCorner.row))
+        elif self.mode==self.CrossMode.Right and track.RightDownCorner:
+            p1=Point(track.RightDownCorner.row,
+                     track.RightDownCorner.col
+                     -0.9*self._get_offset(track,track.RightDownCorner.row))
+        # Don't find corner(s)->use the midpoint of p3 (simplified version) and p0
+        if not p1:
+            temp_p3=track.Longest_White_Line_Top_Point if track.Longest_White_Line_Top_Point else Point(0,w//2)
+            p1_row=Point.get_midpoint(p0,temp_p3).row
+            p1=Point(p1_row,p0.col-10)
+
+        # Determine p3: if the longest white line is available and have a broad view,
+        # use the longest white line end point,
+        # else use the intersection of extension p0p1 and the image top
+        if track.Longest_White_Line_Length>0.5*h:
+            p3=track.Longest_White_Line_Top_Point
+        else:
+            dx,dy=p1.col-p0.col,p1.row-p0.row
+            if dy==0: dy=-0.01  # Avoid division by zero
+            p3_col=p0.col-p0.row*dx/dy
+            p3=Point(0,np.clip(p3_col,0,w-1))
+
+        # Determine p2: The midpoint of p1 and p3
+        p2=Point.get_midpoint(p1,p3)
+
+        # Generate center line
+        track.CenterPoints=Utils.bezier_fit([p0, p1, p2, p3])
+        # Generate side lines (Do not need to do it
         pass
 
     def _check_exit(self,h,w,track,analyse):
@@ -751,7 +796,7 @@ class Cross:
                 "State":"Locked","Time":self._exit_clk
             }
             return False
-        if self._exit_clk>300:  # Force quit on timeout
+        if self._exit_clk>150:  # Force quit on timeout
             self.debug_info={
                 "State":"Timeout","Exit":True
             }
@@ -766,6 +811,7 @@ class Cross:
 
         self.debug_info={
             "State":"InsideCross",
+            'Mode':self.mode.name,
             "GetLine":get_line,
             "NoCorn":down_corners_not_found,
             "LowSt":low_start_row,
@@ -774,8 +820,18 @@ class Cross:
         }
         return should_exit
 
+    def _get_offset(self,track,current_row):
+        # When calculating side lines according to center line,
+        # or determine one of the control point p1,we need to calculate offset
+        if track.start_row is None or self.track_half_width==0:return 0
+        # Assume that image top track width is 0.25 times bottom track width
+        top_track_half_width=0.25*self.track_half_width
+        if track.start_row==0: return top_track_half_width
+        offset=top_track_half_width+current_row*(self.track_half_width-top_track_half_width)/track.start_row
+        return offset
 
-#5.Responsible for visualize everything
+
+#5.Responsible for visualize all elements
 class Visualize:
     def draw_points(self,frame,tracker,crosser):
         # Brief:Visualize everything
@@ -800,7 +856,7 @@ class Visualize:
             p1, p2 = tracker.CenterPoints[i], tracker.CenterPoints[i + 1]
             cv2.line(frame, p1.point2cv(), p2.point2cv(), (0, 0, 255), 2)
 
-        #Draw corners(Big yellow dots with red boarders)
+        #Draw corners(Large yellow dots with red borders)
         corners=[tracker.LeftDownCorner, tracker.RightDownCorner, tracker.LeftUpCorner, tracker.RightUpCorner]
         for p in corners:
             if p is not None:
@@ -840,7 +896,7 @@ class Visualize:
             y=30
             for key,val in crosser.debug_info.items():
                 color=(0,255,0) if val else (0,0,255)   #True: green; False: red
-                if key in ['State','Time']:
+                if key in ['State','Mode','Time']:
                     color=(0,255,255)                   #Txt: Yellow
                     txt=f"{key}:{val}"
                 else:
@@ -848,7 +904,7 @@ class Visualize:
                 # Add a black border,otherwise it'll not clear to see
                 cv2.putText(frame,txt,(x,y),cv2.FONT_HERSHEY_SIMPLEX,font_scale,0,2*font_thickness)
                 cv2.putText(frame,txt,(x,y),cv2.FONT_HERSHEY_SIMPLEX,font_scale,color,font_thickness)
-                y+=30
+                y+=25
 
         return frame
 
@@ -856,6 +912,31 @@ class Visualize:
         self.draw_points(frame, tracker,crosser)
         self.draw_text(h,w,frame,main,tracker,analyser,crosser)
         return frame
+
+
+class Utils:
+    @staticmethod
+    def bezier_fit(input_points,dt=0.01):
+           #
+           # 贝塞尔曲线核心函数
+           # 根据四个控制点生成三次曲线
+           #  Parameters:
+           #     input_points: 输入特征点
+           #  Returns:
+           #     output: 贝塞尔拟合后的点列表
+           #
+        output=[]
+        #检验输入
+        if len(input_points)!=4:
+            print("控制点数量错误")
+            return output
+        t=0
+        while t<=1.0+1e-6:#每个t对应图上一个点,dt控制平滑度,+1e-6是为了确保包含终点（t可能在0.99时t+dt=1.0000000001，跳过t=1.0）
+            center_row= (1 - t) ** 3 * input_points[0].row + 3 * (1 - t) ** 2 * t * input_points[1].row + 3 * (1 - t) * t ** 2 * input_points[2].row + t ** 3 * input_points[3].row
+            center_col= (1 - t) ** 3 * input_points[0].col + 3 * (1 - t) ** 2 * t * input_points[1].col + 3 * (1 - t) * t ** 2 * input_points[2].col + t ** 3 * input_points[3].col
+            output.append(Point(round(center_row), round(center_col)))
+            t+=dt
+        return output
 
 #Orchestrator
 class Main:
@@ -903,6 +984,7 @@ class Main:
                                         self.analyser,
                                         self.crosser
                                         )
+
                 self.latest_valid_frame=display_frame
             if self.latest_valid_frame is not None: #No matter if there is a new frame, we just need a previous valid frame
                 final_show=self.latest_valid_frame.copy()
--- 任务4/learning1.py
+++
@@ -1,5 +0,0 @@
-arry = [0, 1, 2, 3]
-# 打印列表的长度
-print(len(arry))
-print(arry[0])
-print(arry[3])
